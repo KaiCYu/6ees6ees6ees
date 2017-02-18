31af44276294eff238aa253dfa8df084c5e39e3a
KaiCYu <kaichyu@gmail.com>
2017-02-17 23:22:10 -0800

    finished ES6 classing with Bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
-};
+// import Grub from 'Grub';
+
+class Bee extends Grub {
+  constructor () {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'; 
+  }
+}
